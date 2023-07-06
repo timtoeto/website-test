8d06932505b05ba5122ad91bdfbdac95aa22e3be
timtoeto <138740765+timtoeto@users.noreply.github.com>
2023-07-06 10:06:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript code goes here
+// Example: Change the background color on button click
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'pink';
+});
